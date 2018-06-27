b52269e3bacd14405087580408bb1f2bf3c18783
chariot9 <trungvu.inside@gmail.com>
2018-06-27 15:09:13 +0900

    created project

---
+++ resources/views/layouts/container.blade.php
@@ -0,0 +1,14 @@
+<div class="page-container">
+    <div class="page-content">
+        <div class="sidebar sidebar-main">
+            <div class="sidebar-content">
+                @include('layouts.user')
+                @include('layouts.navigation')
+            </div>
+        </div>
+
+        <div class="content-wrapper">
+
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer text-muted">
+    &copy; 2018. <a href="#">Pro News Platform</a> by <a href="https://github.com/chariot9" target="_blank">Trung Vu, inc</a>
+</div>
